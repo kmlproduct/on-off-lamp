855e0c54c15eacf6d05c63558c83d5d08affede6
kmlproduct <kam.salaheddine@gmail.com>
2022-02-14 11:05:28 +0100

    adding lighting to lamp

--- main.js
+++ main.js
@@ -1,16 +1,20 @@
-let imgChange = document.getElementById('imgs');
+// let imgChange = document.getElementById('imgs');
 let on = document.getElementById('btn-on');
 let off = document.getElementById('btn-off');
 
 on.addEventListener('click', function () {
-    let imgChange = document.getElementById('triangle');
-    imgChange.style.display = 'block'
+    let imgChange = document.getElementById('shadow');
+    let imgs = document.getElementById('imgs');
+    imgChange.style.display = 'block';
+    imgs.style.backgroun = 'yellow';
+
 
 
 })
 off.addEventListener('click', function () {
-    let imgChange = document.getElementById('triangle');
-    imgChange.style.display = 'none'
+    let imgChange = document.getElementById('shadow');
+    imgChange.style.display = 'none';
+
 
 
 })
